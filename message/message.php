e7f5e7f87258ce1e039c367adbfdc1bd1bf92010
takuya <uv.nkmt.yii@gmail.com>
2017-11-12 23:51:35 +0800

    update

--- message/message.php
+++ message/message.php
@@ -8,110 +8,14 @@ $user_id = 1 ; //ユーザー名：takuya
 $other_id = 2; //ユーザー名：sample
 
 //初期値の設定
-$user = '';
-$message = '';
 $record = [
 	'user' => '',
 	'message' =>''
 ];
 $inputValue = [];
 
-//main.jsに情報を渡すためのクエリを作成
-$sql = 'SELECT
-		 x.user_id AS "user_id",
-		 NAME AS "user_name",
-		 x.other_id AS "other_id",
-		 batch_users.username AS "other_name"
-		 FROM ( 
-		 	SELECT 
-		 	message.user_id AS "user_id",
-		 	message.other_id AS "other_id",
-		 	batch_users.username AS "NAME"
-		 	FROM message 
-		 	JOIN batch_users 
-		 	ON message.user_id = batch_users.id 
-		 	WHERE message.user_id = ? 
-		 	AND message.other_id = ?
-		 ) x 
-		 JOIN batch_users 
-		 ON x.other_id = batch_users.id
-		;';
-$data = [$user_id,$other_id];
-$stmt = $dbh->prepare($sql);
-$stmt->execute($data);
-$user_info = $stmt->fetch(PDO::FETCH_ASSOC);
-
-// echo "<pre>";
-// var_dump($user_info);
-
-
-
-$sql = 'SELECT
-		 x.user_id AS "user_id",
-		 NAME AS "user_name",
-		 x.other_id AS "other_id",
-		 batch_users.username AS "other_name",
-		 CONTENT AS "content",
-		 x.uplode_image AS "uplode_image"
-		 FROM ( 
-		 	SELECT 
-		 	message.user_id AS "user_id",
-		 	message.other_id AS "other_id",
-		 	batch_users.username AS "NAME",
-		 	message.content AS "CONTENT",
-		 	message.uplode_image AS"uplode_image" 
-		 	FROM message 
-		 	JOIN batch_users 
-		 	ON message.user_id = batch_users.id 
-		 	WHERE (
-		 			message.user_id = ? 
-		 			AND message.other_id = ?
-		 		  ) 
-		 	OR (
-		 			message.user_id = ? 
-		 			AND message.other_id = ?
-		 		  )
-		 ) x 
-		 JOIN batch_users 
-		 ON x.other_id = batch_users.id
-		;';
-			
-$data = [$user_id,$other_id,$other_id,$user_id];
-$stmt = $dbh->prepare($sql);
-$stmt->execute($data);
-// データベース切断
-$dbh = null;
-
-// セレクト文で実行した結果を取得する
-while (true) {
-	$record = $stmt->fetch(PDO::FETCH_ASSOC);
-	if ($record == false) {
-		break;
-	}
-
-	// 投稿は画像かコメントか
-	if ($record['content'] !== 'NULL') {	
-		//投稿はログインユーザーかチェック
-		if ($user_id == $record['user_id']) {
-			$inputValue[] = "<div class='right_balloon'>".$record['content']."</div>";		
-		}else{
-			$inputValue[] = "<div class='left_balloon'>".$record['content']."</div>";		
-		}
-	}else{
-		//投稿はログインユーザーかチェック
-		if ($user_id == $record['user_id']) {
-			$inputValue[] = "<img src=image_uplode/".$record['uplode_image']." class='right_img_uplode'>";		
-		}else{
-			$inputValue[] = "<img src=image_uplode/".$record['uplode_image']." class='left_img_uplode'>";
-		}
-	}
-
-
-	error_log(print_r($inputValue,true),"3","../../../../../logs/error_log");
-}
-
-// echo "<pre>";
-// var_dump($user_info);
+//ユーザーデータ読み出し
+require('model/selectUser.php');
  ?>
 
 <?php require('../part/header.php'); ?>
